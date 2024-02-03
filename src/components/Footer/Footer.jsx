162bd43f51314b04afe6e71bc933aede5097c6ce
Agustin <agustinescalanterojo@gmail.com>
2024-02-03 09:51:45 +0100

    add content in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,27 @@
 import React from "react";
+import styled from "styled-components";
 
 const Footer = () => {
-    return (
-        <StyledFooter>
-            <p>
-                &copy; {new Date().getFullYear()} The Bridge. All rights reserved.
-            </p>
-        </StyledFooter>
-    );
+  return (
+    <Container>
+      <Profile>
+        <h1>Portfolio</h1>
+        <div className="links">
+          <h1>Contact me directly:</h1>
+          <div>
+            <span>
+
+            </span>
+          </div>
+        </div>
+      </Profile>
+      <Form></Form>
+    </Container>
+  );
 };
 
-export default Footer
+export default Footer;
+
+const Container = styled.div``;
+const Profile = styled.div``;
+const Form = styled.div``;
